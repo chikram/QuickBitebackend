aea6d00cac85d66e12dc92415b82a07470cf7a36
Ikram <ikramaslam1020@gmail.com>
2024-04-17 22:33:19 +0500

    creating delete item api

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 //add food item
 const addFood = async (req, res) => {
 
-    let image_filename = `${req.file.image_filename}`;
+    let image_filename = `${req.file.filename}`;
 
     const food = new foodModel({
         name: req.body.name,
@@ -43,7 +43,20 @@ const listFood = async (req, res) => {
 
 //remove food item
 const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => { })
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Item Removed" })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
+    }
 
 }
+
+
 
 export { addFood, listFood, removeFood }
